ad16e781a24ad7fc4a49c92c2785950cbc4e1d86
Anomitra Saha <anomitra1993@gmail.com>
2015-02-09 09:16:48 +0000

    [FIX] Change the 'ques' model to Question. Rename.
    * All migrations done.
    
    * Foreign keys fixed.

--- polls/models.py
+++ polls/models.py
@@ -1,11 +1,21 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 
-class ques(models.Model):
+class Question(models.Model):
     ques_text=models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+    def __str__(self):
+        return self.ques_text
+    def recent(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 
 class Choice(models.Model):
-    ques = models.ForeignKey(ques)
+    ques = models.ForeignKey(Question)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+    def __str__(self):
+        vote=str(self.votes)
+        retstr=self.choice_text+" -- "+vote
+        return retstr
